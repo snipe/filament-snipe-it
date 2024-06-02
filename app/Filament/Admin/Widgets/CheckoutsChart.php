599d142ce67878397edb80bc72de523aa6e8b144
snipe <snipe@snipe.net>
2024-06-02 16:57:11 +0100

    Added checkout chart
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ app/Filament/Admin/Widgets/CheckoutsChart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Asset;
+
+class CheckoutsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Checkout Trends';
+    public ?string $filter = 'today';
+    protected function getData(): array
+    {
+        $activeFilter = $this->filter;
+
+        $data = Trend::model(Asset::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->dateColumn('last_checkout')
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Asset Checkouts',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'Today',
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
+        ];
+    }
+}
