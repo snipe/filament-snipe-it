e3927dc979dbcf139229852ef2705a6389d6c738
snipe <snipe@snipe.net>
2024-06-02 17:49:36 +0100

    Added page components
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ resources/views/filament/admin/pages/assets.blade.php
@@ -0,0 +1,7 @@
+<x-filament-panels::page>
+    <x-filament::breadcrumbs :breadcrumbs="[
+    '/' => 'Dashboard',
+    '/assets' => 'Assets',
+    '/create' => 'Create User',
+]" />
+</x-filament-panels::page>
