23311e4dc2de487a3d4c79b29d9d82b363ca4840
snipe <snipe@snipe.net>
2024-06-02 19:41:18 +0100

    Added breadcrumbs
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/filament/admin/pages/assets.blade.php
+++ resources/views/filament/admin/pages/assets.blade.php
@@ -1,3 +1,7 @@
 <x-filament-panels::page>
-
+    <x-filament::breadcrumbs :breadcrumbs="[
+    '/' => 'Dashboard',
+    '/users' => 'Assets',
+    '/create' => 'Create Asset',
+]" />
 </x-filament-panels::page>
