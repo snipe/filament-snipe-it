1e9e640d1d290451075eb0648c68b169475f0bf0
snipe <snipe@snipe.net>
2024-06-02 16:57:45 +0100

    Added sidebarCollapsibleOnDesktop to panel
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -23,6 +23,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+            ->sidebarCollapsibleOnDesktop()
             ->id('admin')
             ->path('admin')
             ->colors([
