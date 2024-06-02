0ee56ab51697b04b9db3263f8fe88fb190c1b42d
snipe <snipe@snipe.net>
2024-06-02 19:41:39 +0100

    Added userlink
    This needs work - font is too big
    
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Tables/Columns/UserLink.php
+++ app/Tables/Columns/UserLink.php
@@ -3,8 +3,25 @@
 namespace App\Tables\Columns;
 
 use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Str;
 
-class UserLink extends Column
+class UserLink extends TextColumn
 {
     protected string $view = 'tables.columns.user-link';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->url(function ($record) {
+            if ($record === null) {
+                return null;
+            }
+
+            return '<a href="mailto:'.$record->email.'">'.$record->email.'</a>';
+
+        });
+    }
+
 }
