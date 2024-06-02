2357ac2400bc21c40966602656920b7459cd1b33
snipe <snipe@snipe.net>
2024-06-02 18:08:49 +0100

    Added breadcrumbs
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/filament/admin/pages/users.blade.php
+++ resources/views/filament/admin/pages/users.blade.php
@@ -1,3 +1,7 @@
 <x-filament-panels::page>
-
+    <x-filament::breadcrumbs :breadcrumbs="[
+    '/' => 'Dashboard',
+    '/users' => 'Users',
+    '/create' => 'Create User',
+]" />
 </x-filament-panels::page>
