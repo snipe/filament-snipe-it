e1b2a7bbe7b906995cf451345b632a711500a1fa
snipe <snipe@snipe.net>
2024-06-02 19:42:27 +0100

    Changed sorting of widgets
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Widgets/AssetListing.php
+++ app/Filament/Admin/Widgets/AssetListing.php
@@ -5,6 +5,7 @@ namespace App\Filament\Admin\Widgets;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Table;
@@ -16,7 +17,7 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class AssetListing extends BaseWidget
 {
-    protected static ?int $sort = 5;
+    protected static ?int $sort = 15;
     protected int | string | array $columnSpan = 'full';
 
     public function table(Table $table): Table
@@ -24,14 +25,15 @@ class AssetListing extends BaseWidget
         return $table
             ->query(Asset::query())
             ->columns([
-                TextColumn::make('asset_tag'),
-                TextColumn::make('name'),
-                TextColumn::make('serial'),
-                TextColumn::make('purchase_cost'),
-                IconColumn::make('requestable')->boolean(),
-                TextColumn::make('asset_tag'),
-                TextColumn::make('purchase_date'),
-                IconColumn::make('assigned_to')->boolean()->label('Checked Out'),
+                TextColumn::make('id')->toggleable()->sortable(),
+                TextColumn::make('asset_tag')->sortable(),
+                TextColumn::make('name')->sortable(),
+                TextColumn::make('serial')->copyable()->sortable(),
+                TextColumn::make('purchase_cost')->money('EUR', locale: 'pt')->sortable(),
+                IconColumn::make('requestable')->boolean()->sortable(),
+                TextColumn::make('asset_tag')->sortable(),
+                TextColumn::make('purchase_date')->dateTime($format = 'F j, Y H:i:s')->sortable(),
+                IconColumn::make('assigned_to')->boolean()->label('Checked Out')->sortable(),
 
             ])
             ->filters([
@@ -42,6 +44,7 @@ class AssetListing extends BaseWidget
 
             ->actions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->bulkActions([
                 BulkActionGroup::make([
