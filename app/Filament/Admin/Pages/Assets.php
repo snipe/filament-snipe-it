92064afe8985e9c9475d59f38b2b33b47ccf5823
snipe <snipe@snipe.net>
2024-06-02 19:43:03 +0100

    Use widget footer instead
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Pages/Assets.php
+++ app/Filament/Admin/Pages/Assets.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Admin\Pages;
 
 use Filament\Pages\Page;
+use App\Filament\Admin\Widgets\AssetListing;
 
 class Assets extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.admin.pages.assets';
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            AssetListing::class
+        ];
+    }
 }
