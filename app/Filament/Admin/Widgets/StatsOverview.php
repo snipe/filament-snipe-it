07e0552156e7497d83197c8a28f9bfcf5371556a
snipe <snipe@snipe.net>
2024-06-02 19:41:48 +0100

    Change some of the stats
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Widgets/StatsOverview.php
+++ app/Filament/Admin/Widgets/StatsOverview.php
@@ -5,26 +5,29 @@ namespace App\Filament\Admin\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Asset;
+use App\Models\User;
 
 class StatsOverview extends BaseWidget
 {
-
+    protected static ?int $sort = 1;
     protected function getStats(): array
     {
         return [
-            Stat::make('Assets', number_format(Asset::count(),))
+            Stat::make('Assets', number_format(Asset::count()))
                 ->description('32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
-            Stat::make('Bounce rate', '21%')
+            Stat::make('People', number_format(User::count()))
                 ->description('7% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('danger'),
+            Stat::make('Some other stat', '3:12')
+                ->description('3% decrease')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger'),
-            Stat::make('Average time on page', '3:12')
-                ->description('3% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success'),
         ];
     }
+
+
 }
