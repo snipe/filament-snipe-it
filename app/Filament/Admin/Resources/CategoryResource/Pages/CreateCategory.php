999cd3b0735ecfc5181752c2b2fdaf43f2c72c8b
snipe <snipe@snipe.net>
2024-06-06 17:58:52 +0100

    Removed actions
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Admin\Resources\CategoryResource\Pages;
 
 use App\Filament\Admin\Resources\CategoryResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCategory extends CreateRecord
