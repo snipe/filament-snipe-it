cb6d92bdf0880bd77c237c7eaee026671d73bfab
snipe <snipe@snipe.net>
2024-06-06 13:09:57 +0100

    Fleshed out accessory schema and table
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Resources/AccessoryResource.php
+++ app/Filament/Admin/Resources/AccessoryResource.php
@@ -6,6 +6,7 @@ use App\Filament\Admin\Resources\AccessoryResource\Pages;
 use App\Filament\Admin\Resources\AccessoryResource\RelationManagers;
 use App\Models\Accessory;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -15,10 +16,19 @@ use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Admin\Widgets\StatsOverview;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\FileUpload;
+use App\Models\Category;
+use App\Models\Manufacturer;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Checkbox;
 
 class AccessoryResource extends Resource
 {
@@ -30,7 +40,29 @@ class AccessoryResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->autofocus()
+                    ->maxLength(255),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->options(Category::where('category_type','accessory')->pluck('name', 'id'))
+                    ->searchable()
+                    ->native(false),
+                Select::make('manufacturer_id')
+                    ->label('Manufacturer')
+                    ->options(Manufacturer::all()->pluck('name', 'id'))
+                    ->searchable()
+                    ->native(false),
+                TextInput::make('email')
+                    ->maxLength(255),
+                TextInput::make('phone')
+                    ->maxLength(255),
+                TextInput::make('jobtitle')
+                    ->maxLength(255),
+                FileUpload::make('image'),
+                Textarea::make('notes'),
+                Checkbox::make('requestable')->inline()
             ]);
     }
 
@@ -40,9 +72,16 @@ class AccessoryResource extends Resource
             ->columns([
                 TextColumn::make('id')->toggleable()->sortable(),
                 TextColumn::make('name')->toggleable()->sortable(),
+                TextColumn::make('model_number')->toggleable()->sortable(),
+                TextColumn::make('category')->toggleable()->sortable(),
+                TextColumn::make('qty')->toggleable()->sortable(),
+                TextColumn::make('min_amt')->toggleable()->sortable(),
+                TextColumn::make('admin')->toggleable()->sortable(),
                 TextColumn::make('purchase_cost')->toggleable()->money('EUR', locale: 'pt')->sortable(),
-                TextColumn::make('purchase_date')->toggleable()->dateTime($format = 'F j, Y H:i:s')->sortable(),
-
+                TextColumn::make('purchase_date')->toggleable()->dateTime($format = 'F j, Y')->sortable(),
+                TextColumn::make('order_number')->toggleable()->sortable(),
+                ToggleColumn::make('requestable')->toggleable(isToggledHiddenByDefault: true)->sortable(),
+                TextColumn::make('updated_at')->toggleable()->dateTime($format = 'F j, Y H:i:s')->sortable(),
             ])
             ->actions([
                 EditAction::make(),
@@ -72,6 +111,8 @@ class AccessoryResource extends Resource
             'edit' => Pages\EditAccessory::route('/{record}/edit'),
         ];
     }
+
+
 
 
 }
