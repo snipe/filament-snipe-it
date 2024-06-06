2410aa0f4ce353b3735625de0ef5c4fd8b2c5ce4
snipe <snipe@snipe.net>
2024-06-06 13:58:30 +0100

    Added model relationship
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -11,4 +11,16 @@ class Asset extends Model
 
     protected $table = 'assets';
 
+    /**
+     * Establishes the asset -> model relationship
+     *
+     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @since [v1.0]
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function model()
+    {
+        return $this->belongsTo(\App\Models\AssetModel::class, 'model_id');
+    }
+
 }
