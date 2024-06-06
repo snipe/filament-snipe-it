567ce8795511c3487bc15a29a3ab70538b7098da
snipe <snipe@snipe.net>
2024-06-06 16:21:51 +0100

    Added deployable scope - I don’t know if we actually need this tho
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/StatusLabel.php
+++ app/Models/StatusLabel.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class StatusLabel extends Model
 {
     use HasFactory;
+
+    /**
+     * Query builder scope for deployable status types
+     *
+     * @return \Illuminate\Database\Query\Builder Modified query builder
+     */
+    public function scopeDeployable()
+    {
+        return $this->where('pending', '=', 0)
+            ->where('archived', '=', 0)
+            ->where('deployable', '=', 1);
+    }
 }
