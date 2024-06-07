d71e4b8d7a908a3a36edecb95434630e608f4da0
snipe <snipe@snipe.net>
2024-06-07 14:47:31 +0100

    Messing around with clusters
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -3,12 +3,14 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
+use App\Filament\Clusters\Users;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+    protected static ?string $cluster = Users::class;
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Admin/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ViewUser.php
@@ -8,36 +8,47 @@ use Filament\Resources\Pages\ViewRecord;
 use Filament\Forms\Components\Tabs;
 use Filament\Support\Enums\IconPosition;
 use Filament\Resources\Concerns\HasTabs;
+use App\Models\User;
+use App\Filament\Clusters\Users;
 
 class ViewUser extends ViewRecord
 {
     protected static string $resource = UserResource::class;
-    protected static string $view = 'filament.resources.users.pages.view-user';
+    protected static ?string $cluster = Users::class;
+
+    // protected static string $view = 'filament.resources.users.pages.view-user';
     use HasTabs;
 
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+            Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
 
-    /** this doesn't work yet */
-    public static function tabs() {
-        return Tabs::make('Tabs')
-            ->tabs([
-                Tabs\Tab::make('Tab 1')
-                    ->icon('heroicon-m-bell')
-                    ->badge(5)
-                    ->badgeColor('success')
-                    ->schema([
-                        // ...
-                    ]),
-                Tabs\Tab::make('Tab 2')
-                    ->badge(12)
-                    ->schema([
-                        // ...
-                    ]),
-                Tabs\Tab::make('Tab 3')
-                    ->schema([
-                        // ...
-                    ]),
-            ])
-            ->activeTab(2)
-            ->persistTabInQueryString();
+
+    public function getTabs(): array
+    {
+        $tabs = ['all' => Tabs::make('All')->badge($this->getModel()::count())];
+
+        $users = User::orderBy('id', 'asc')
+            ->withCount('assets')
+            ->withCount('accessories')
+            ->get();
+
+        foreach ($users as $user) {
+            $name = $user->name;
+            $slug = str($name)->slug()->toString();
+
+            $tabs[$slug] = Tabs::make($name)
+                ->badge($user->assets)
+                ->modifyQueryUsing(function ($query) use ($user) {
+                    return $query->where('id', $user->id);
+                });
+        }
+
+        return $tabs;
     }
 }
