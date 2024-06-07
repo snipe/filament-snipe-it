ef4cb8410ffb955a2fd83cf0c5ff05896836730a
snipe <snipe@snipe.net>
2024-06-07 23:24:36 +0100

    Added fillable for department
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -9,6 +9,17 @@ class Department extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'fax',
+        'location_id',
+        'company_id',
+        'manager_id',
+        'notes',
+    ];
+
     public function admin() {
         return $this->belongsTo(User::class, 'user_id');
     }
