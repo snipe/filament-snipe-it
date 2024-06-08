621e67f58ab2eaa3d3629457bd6de4198432a390
snipe <snipe@snipe.net>
2024-06-08 02:18:43 +0100

    Added company to settings grounp
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Resources/CompanyResource.php
+++ app/Filament/Admin/Resources/CompanyResource.php
@@ -18,7 +18,9 @@ class CompanyResource extends Resource
 {
     protected static ?string $model = Company::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Settings';
+
+    protected static ?string $navigationIcon = null;
 
     public static function form(Form $form): Form
     {
