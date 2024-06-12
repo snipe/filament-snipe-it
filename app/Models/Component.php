60d12c4ac0727547654c8a9d89e8205a107bdb54
snipe <snipe@snipe.net>
2024-06-12 16:06:36 +0100

    Added rules
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Component.php
+++ app/Models/Component.php
@@ -13,6 +13,37 @@ class Component extends Model
     use ValidatingTrait;
     use SoftDeletes;
 
+    public $rules = [
+        'name'           => 'required|min:3|max:255',
+        'qty'            => 'required|integer|min:1',
+        'category_id'    => 'required|integer|exists:categories,id',
+        'supplier_id'    => 'nullable|integer|exists:suppliers,id',
+        'company_id'     => 'integer|nullable|exists:companies,id',
+        'min_amt'        => 'integer|min:0|nullable',
+        'purchase_date'   => 'date_format:Y-m-d|nullable',
+        'purchase_cost'  => 'numeric|nullable|gte:0',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'company_id',
+        'supplier_id',
+        'location_id',
+        'name',
+        'purchase_cost',
+        'purchase_date',
+        'min_amt',
+        'order_number',
+        'qty',
+        'serial',
+        'notes',
+    ];
+
     public function admin() {
         return $this->belongsTo(User::class, 'user_id');
     }
