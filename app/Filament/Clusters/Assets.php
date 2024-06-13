6bb52807eccc9fdfd3dd451532b440ac7e93ee9c
snipe <snipe@snipe.net>
2024-06-13 22:16:26 +0100

    Added higher level badge count
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Clusters/Assets.php
+++ app/Filament/Clusters/Assets.php
@@ -3,9 +3,14 @@
 namespace App\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
+use App\Models\Asset;
 
 class Assets extends Cluster
 {
     protected static ?string $navigationIcon = 'fas-barcode';
+    public static function getNavigationBadge(): ?string
+    {
+        return Asset::count();
+    }
 
 }
