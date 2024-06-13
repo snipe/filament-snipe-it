431f423590847b8e11d58a0b7e043b22da07063f
snipe <snipe@snipe.net>
2024-06-14 00:40:03 +0100

    Added wanr debug to app config
    Signed-off-by: snipe <snipe@snipe.net>

--- config/app.php
+++ config/app.php
@@ -40,6 +40,7 @@ return [
     */
 
     'debug' => (bool) env('APP_DEBUG', false),
+    'warn_debug' => env('WARN_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
