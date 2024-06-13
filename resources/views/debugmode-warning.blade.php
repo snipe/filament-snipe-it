3889f4e88b2c12c4ddc3c35d5f3c121a37e36bcd
snipe <snipe@snipe.net>
2024-06-14 00:39:20 +0100

    Updated alert
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/debugmode-warning.blade.php
+++ resources/views/debugmode-warning.blade.php
@@ -1,7 +1,5 @@
 <div class="bg-red-700 border border-red-400 text-white-700 mt-6 px-4 py-3 rounded relative" role="alert">
-    <strong class="font-bold">WARNING</strong>
-    <span class="block sm:inline">Debug mode is enabled.</span>
-    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-  </span>
+    <strong class="font-bold text-white-500">WARNING</strong>
+    <span class="block sm:inline">Debug mode is enabled while this app is in production mode. This can leak sensitive information.</span>
 </div>
 
