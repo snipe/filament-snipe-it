421f8cef0e589775db12d69fd7cee10092df9f92
snipe <snipe@snipe.net>
2024-06-14 13:09:01 +0100

    Comment out the tabs for now - they’re cluttery
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -21,18 +21,18 @@ class ListUsers extends ListRecords
         ];
     }
 
-    public function getTabs(): array
-    {
-        return [
-            'all' => Tab::make(),
-            'active' => Tab::make()
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('activated', true)),
-            'inactive' => Tab::make()
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('activated', false)),
-            'deleted' => Tab::make()
-                ->modifyQueryUsing(fn (Builder $query) => $query->onlyTrashed()),
-        ];
-    }
+//    public function getTabs(): array
+//    {
+//        return [
+//            'all' => Tab::make(),
+//            'active' => Tab::make()
+//                ->modifyQueryUsing(fn (Builder $query) => $query->where('activated', true)),
+//            'inactive' => Tab::make()
+//                ->modifyQueryUsing(fn (Builder $query) => $query->where('activated', false)),
+//            'deleted' => Tab::make()
+//                ->modifyQueryUsing(fn (Builder $query) => $query->onlyTrashed()),
+//        ];
+//    }
 
     protected function getTableRecordsPerPageSelectOptions(): array
     {
