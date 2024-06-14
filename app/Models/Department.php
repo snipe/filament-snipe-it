575dfef8e6d4dbdf724cb8973e1336b63d076d64
snipe <snipe@snipe.net>
2024-06-14 03:09:59 +0100

    Added relationships
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -27,4 +27,78 @@ class Department extends Model
     public function admin() {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    /**
+     * Establishes the department -> company relationship
+     *
+     * @author A. Gianotto <snipe@snipe.net>
+     * @since [v4.0]
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function company()
+    {
+        return $this->belongsTo(\App\Models\Company::class, 'company_id');
+    }
+
+    /**
+     * Establishes the department -> users relationship
+     *
+     * @author A. Gianotto <snipe@snipe.net>
+     * @since [v4.0]
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function users()
+    {
+        return $this->hasMany(\App\Models\User::class, 'department_id');
+    }
+
+    /**
+     * Establishes the department -> manager relationship
+     *
+     * @author A. Gianotto <snipe@snipe.net>
+     * @since [v4.0]
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function manager()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'manager_id');
+    }
+
+    /**
+     * Establishes the department -> location relationship
+     *
+     * @author A. Gianotto <snipe@snipe.net>
+     * @since [v4.0]
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function location()
+    {
+        return $this->belongsTo(\App\Models\Location::class, 'location_id');
+    }
+
+    /**
+     * Query builder scope to order on location name
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query  Query builder instance
+     * @param  text                              $order       Order
+     *
+     * @return \Illuminate\Database\Query\Builder          Modified query builder
+     */
+    public function scopeOrderLocation($query, $order)
+    {
+        return $query->leftJoin('locations as department_location', 'departments.location_id', '=', 'department_location.id')->orderBy('department_location.name', $order);
+    }
+
+    /**
+     * Query builder scope to order on manager name
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query  Query builder instance
+     * @param  text                              $order       Order
+     *
+     * @return \Illuminate\Database\Query\Builder          Modified query builder
+     */
+    public function scopeOrderManager($query, $order)
+    {
+        return $query->leftJoin('users as department_user', 'departments.manager_id', '=', 'department_user.id')->orderBy('department_user.first_name', $order)->orderBy('department_user.last_name', $order);
+    }
 }
