96a9cb04f23e784997347c7c23871d25c11ab274
snipe <snipe@snipe.net>
2024-06-14 02:31:46 +0100

    Added fillable to consumable model
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Consumable.php
+++ app/Models/Consumable.php
@@ -9,6 +9,23 @@ class Consumable extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'category_id',
+        'company_id',
+        'item_no',
+        'location_id',
+        'manufacturer_id',
+        'name',
+        'order_number',
+        'model_number',
+        'purchase_cost',
+        'purchase_date',
+        'qty',
+        'min_amt',
+        'requestable',
+        'notes',
+    ];
+
     public function admin() {
         return $this->belongsTo(User::class, 'user_id');
     }
